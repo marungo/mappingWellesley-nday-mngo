cc54a7b34a2d643b4ec6159ae38468f2eba005fa
Mary Ruth Ngo <mngo@wellesley.edu>
2017-04-17 23:29:57 -0400

    added marker capabilities

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 # hwk6: MR Ngo
 from __future__ import print_function
 from flask import Flask, render_template, redirect, url_for, request, flash
+# from flask_googlemaps import GoogleMaps
 import os
 import sys
 # my own set of mysql query and statement functions
 
 app = Flask(__name__)
 app.secret_key = '39tsfkajie' # for flashing
+# GoogleMaps(app, key='AIzaSyA-nT9fP4I7GrFPu_J-V-5ajx1Esns2aNk')
 
 @app.route('/', methods=['POST', 'GET'])
 def base():
@@ -16,3 +18,4 @@ if __name__ == '__main__':
 	app.debug = True
 	# app.run('0.0.0.0', os.getuid())
 	app.run()
+
